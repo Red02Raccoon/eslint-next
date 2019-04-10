01e6361e6f0a69d2a5f0c1aaeb3173bd601a81ea
NastyaCh <nastya02chervona@gmail.com>
2019-04-10 11:10:18 +0300

    option_2

---
+++ index.js
@@ -0,0 +1,10 @@
+var a = 3
+
+const b = 5
+
+console.log('df')
+;('Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus eius distinctio optio odio ratione. Commodi obcaecati aut facere expedita reiciendis facilis perferendis est incidunt veniam officia earum nobis sit, error nostrum voluptas. Velit magnam, necessitatibus expedita debitis, omnis, sapiente rem consequuntur perferendis consequatur ut labore maiores. Ipsa consequuntur excepturi harum quo cupiditate, quibusdam recusandae sed tempora pariatur deleniti ut repudiandae iste officiis minus. Placeat ipsam ratione quam alias culpa adipisci, magni cumque, nisi enim molestiae rem ad in odio obcaecati suscipit impedit animi perspiciatis et repudiandae dignissimos est. Quam veritatis delectus totam similique quas nesciunt sed numquam eos ipsum nulla, id architecto nemo in natus harum ipsam placeat. Obcaecati repudiandae earum voluptatibus dolores architecto culpa et error amet maxime? Tenetur dolorum repudiandae natus rerum cupiditate adipisci eligendi nesciunt dolore iure ab? Aspernatur sequi aliquam quas amet! Iusto, facere. Nisi cum incidunt debitis adipisci repellat voluptas ipsam alias ex nulla distinctio eveniet vitae corporis veniam nihil laboriosam itaque odit, similique illo quisquam perferendis ratione, culpa nesciunt ducimus? Dignissimos eaque placeat aliquid deserunt veritatis atque qui accusantium. Iste praesentium eligendi, culpa, enim accusamus corrupti dolorum alias, aut quo voluptatum dicta? Quaerat rem doloribus dignissimos alias, officia vel consequuntur voluptatum nobis temporibus velit.')
+
+if (b) {
+  return a
+}
--- lint-staged.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  linters: {
-    '*.js': ['eslint --fix', 'prettier --write', 'git add'],
-  },
-  ignore: ['**/dist/*.min.js'],
-}
